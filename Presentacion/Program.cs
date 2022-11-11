8eb8adc971bb4ced5b1da9dd416f1a97ca7a3f9c
Everjam28 <82244064+Everjam28@users.noreply.github.com>
2022-11-11 09:19:09 -0500

    Update Program.cs

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -19,7 +19,7 @@ namespace Presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmListadoClientes());
+            Application.Run(new FrmMenu());
         }
     }
 }
